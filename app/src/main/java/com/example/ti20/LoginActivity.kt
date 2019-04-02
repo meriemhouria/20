521572c9d5ef5976f06ace820f49524a3cb33150
ilyess96 <lyes.mrbnt@gmail.com>
2019-04-02 13:40:11 +0200

    kotlin
    projet android

---
+++ app/src/main/java/com/example/ti20/LoginActivity.kt
@@ -0,0 +1,25 @@
+package com.example.ti20
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity: AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        login_button_login.setOnClickListener {
+            val email = email_edittext_login.text.toString()
+            val password = password_edittext_login.text.toString()
+
+            Log.d("Login", "Attempt login with email/pw: $email/***")
+        }
+       back_to_register_text_view.setOnClickListener {
+           finish()
+       }
+    }
+}
